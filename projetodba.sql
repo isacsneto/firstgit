7596b229c0239a1238e11355e6e54a7fc7fe85a1
Isac Santos <38865175+isacsneto@users.noreply.github.com>
2019-05-17 16:11:11 -0300

    Bando de dados para funcionamento do programa.

---
+++ projetodba.sql
@@ -0,0 +1,77 @@
+create database projeto;
+
+create table cliente(
+
+nome varchar(50), 
+cpf char(14) primary key, 
+email varchar(50), 
+celular char(15), 
+cep char(9), 
+rua varchar(100), 
+numero varchar(5)
+
+);
+
+create table fornecedor(
+
+nome varchar(100), 
+cnpj char(18), 
+email varchar(50), 
+celular char(15), 
+cep char(9), 
+rua varchar(100), 
+numero varchar(5)
+
+);
+
+create table produtos(
+
+codigo int primary key auto_increment, 
+nome varchar(50), 
+preco double(15,2), 
+qtd int, 
+descricao varchar(100), 
+data_fabri char(10), 
+cnpj_fornec char(18)
+
+);
+
+create table funcionario (
+
+nome varchar(50),  
+cpf char(14),  
+email varchar(50), 
+datanascimento char(10),  
+celular char(15),  
+cep char(9),  
+rua varchar(100), 
+numero varchar(5),  
+funcao varchar(12), 
+senha varchar(16),  
+primary key (cpf)
+
+);
+
+create table vendas (
+
+idvenda int not null auto_increment primary key,
+clientev char(14),
+qtditens int,
+datavenda char(10),
+valort double,
+FOREIGN KEY (clientev)
+REFERENCES cliente(cpf)
+
+);
+
+create table vendasprodutos (
+
+codvenda int,
+codproduto int not null,
+qtdproduto int,
+FOREIGN KEY (codvenda)
+REFERENCES vendas(idvenda),
+FOREIGN KEY (codproduto)
+REFERENCES produtos(codigo)
+
+);
